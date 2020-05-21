9aabd378ca3e3652c782b91e496a68589d9f4ed4
Seth Nelson <seth.nelson@outlook.com>
2020-05-20 17:57:13 -0700

    got all queries complete except for the order by orderdate and stretch for stored cities

--- queries.sql
+++ queries.sql
@@ -2,14 +2,14 @@
 
 -- BASIC STRUCTURES:
 
-    -- GET * FROM ___ {gets all of the inout values}
+    -- GET * FROM ___ {gets all of the input values}
 
     -- SELECT <selection> FROM <table name>;
 
         -- SELECT <selection1>, <selection2> FROM <table name>
 
         -- SELECT * FROM Customers
-        -- WHERE CustomerId=3;
+        -- WHERE CustomerId = 3;
 
         -- SELECT * FROM employees 
         -- WHERE salary >= 50000
@@ -43,17 +43,32 @@
 -------------------------------------------------------------
 
 -- Find all customers with postal code 1010
+    SELECT * FROM Customers
+    WHERE PostalCode = 1010
 
 -- Find the phone number for the supplier with the id 11
+    SELECT PhoneNumber FROM Suppliers
+    WHERE SupplierID = 11
 
 -- List first 10 orders placed, sorted descending by the order date
+    ???
 
 -- Find all customers that live in London, Madrid, or Brazil
+    SELECT * FROM Customers
+    WHERE City = 'London' OR City = 'Madrid' OR Country = 'Brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
+    INSERT INTO Customers (ContactName, Address, City, PostalCode, Country)
+    VALUES ('Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+    UPDATE Customers
+    SET PostalCode = 11122
+    WHERE ContactName = 'Bilbo Baggins'
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+    SELECT * FROM Suppliers
+    WHERE length(SupplierName) > 20
