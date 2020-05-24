74082ed0a71c4641612d37f12db1d7c096e80bcf
Seth Nelson <seth.nelson@outlook.com>
2020-05-24 11:18:28 -0700

    finished put and delete endpoints

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -64,14 +64,35 @@ router.post('/', async (req, res) => {
 });
 
 
-router.put('/:id', async (req, res) => {
-    
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    db('accounts').where({ id }).update(changes)
+        .then(count => {
+            if (count) {
+                res.status(200).json({updated: count});
+            } else {
+                res.status(404).json({ message: 'invalid id.' });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Problem with the db.' });
+        })
 })
 
 
 router.delete('/:id', async (req, res) => {
-    
-})
+    const { id } = req.params;
+
+    try {
+        const count = await db.del().from('accounts').where({ id });
+        count ? res.status(200).json({ deleted: count })
+            : res.status(404).json({ message: 'invalid id.'});
+    } catch (err) {
+        res.status(500).json({ message: 'database error', error: err });
+    }
+});
 
 
 module.exports = router;
